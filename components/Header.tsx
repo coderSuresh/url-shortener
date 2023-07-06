94e3779f1abaaceb1e15e15cce2743ed5484d5e7
codersuresh <sd807141@gmail.com>
2023-07-06 14:11:11 +0545

    feat: add mobile menu layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,40 +5,52 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className='px-10 py-4'>
-            <nav className='flex relative justify-start items-center '>
-                    <Link href="/">
-                        <Image src="/images/logo.svg" width="100" height="30" alt='logo' />
-                    </Link>
+        <header className='sm:px-10 py-4 px-5 flex relative justify-start items-center'>
+            <Link href="/">
+                <Image src="/images/logo.svg" width="100" height="30" alt='logo' />
+            </Link>
+            <nav className='
+                flex sm:flex-row 
+                sm:text-left text-center
+                sm:w-fit w-[calc(100%-40px)]
+                flex-col fixed sm:static
+                top-14 items-center
+                justify-between
+                sm:bg-transparent bg-dark-violet
+                sm:p-0 p-5
+                rounded-lg
+            '>
 
-                    <ul className="ml-10 flex gap-5">
-                        <li>
-                            <Link href="/features">
-                                <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Features</p>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/pricing">
-                                <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Pricing</p>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/resources">
-                                <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Resources</p>
-                            </Link>
-                        </li>
-                    </ul>
-
-                <ul className='flex items-center gap-5 absolute right-0'>
+                <ul className="sm:ml-10 flex sm:flex-row flex-col sm:gap-5 gap-3">
                     <li>
-                        <Link href="/login">
-                            <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Login</p>
+                        <Link href="/features">
+                            <p className="sm:text-grayish-violet text-white font-bold text-sm sm:hover:text-very-dark-violet">Features</p>
                         </Link>
                     </li>
                     <li>
+                        <Link href="/pricing">
+                            <p className="sm:text-grayish-violet text-white font-bold text-sm sm:hover:text-very-dark-violet">Pricing</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/resources">
+                            <p className="sm:text-grayish-violet text-white font-bold text-sm sm:hover:text-very-dark-violet">Resources</p>
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="sm:hidden block divider h-[1px] w-full my-3 bg-grayish-violet" />
+
+                <ul className='flex sm:w-fit w-full items-center sm:flex-row flex-col sm:gap-5 gap-3 sm:absolute sm:right-10 right-5'>
+                    <li>
+                        <Link href="/login">
+                            <p className="sm:text-grayish-violet text-white font-bold text-sm sm:hover:text-very-dark-violet">Login</p>
+                        </Link>
+                    </li>
+                    <li className='w-full'>
                         <Link href="/signup">
                             <button
-                                className="bg-primary hover:opacity-60 text-white font-bold text-sm px-4 py-2 rounded-full hover:bg-opacity-80">
+                                className="bg-primary w-full hover:opacity-60 text-white font-bold text-sm px-4 py-2 rounded-full hover:bg-opacity-80">
                                 Sign Up
                             </button>
                         </Link>
