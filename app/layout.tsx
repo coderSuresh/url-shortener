ae5fae06fd96f392fff9a917d7af28a6c3480316
codersuresh <sd807141@gmail.com>
2023-07-06 12:26:20 +0545

    setup styles and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ type Props = {
 const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      </head>
       <body>{children}</body>
     </html>
   )
