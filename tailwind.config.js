11571e5da86f78f38f90ee8d51c3ae0908f29b76
codersuresh <sd807141@gmail.com>
2023-07-06 12:40:02 +0545

    extend custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary': 'var(--primary)',
+        'dark-violet': 'var(--dark-violet)',
+        'very-dark-blue': 'var(--very-dark-blue)',
+        'danger': 'var(--danger)',
+        'gray': 'var(--gray)',
+        'grayish-violet': 'var(--grayish-violet)',
+        'very-dark-violet': 'var(--very-dark-violet)',
       },
     },
   },
