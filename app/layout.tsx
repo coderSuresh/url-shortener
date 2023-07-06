54c7325e9612fbc3a6ff7de7cd3a6ed337cfa3a9
codersuresh <sd807141@gmail.com>
2023-07-06 13:24:59 +0545

    rafactor: separate header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Header from '@/components/Header'
 import './globals.css'
 
 export const metadata = {
@@ -19,51 +18,7 @@ const RootLayout = ({ children }: Props) => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
-
-        <header className='px-10 py-4'>
-          <nav className='flex justify-between'>
-            <div className="logo--side flex items-center">
-              <Link href="/">
-                <Image src="/images/logo.svg" width="100" height="30" alt='logo' />
-              </Link>
-
-              <ul className="ml-10 flex gap-5">
-                <li>
-                  <Link href="/features">
-                    <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Features</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/pricing">
-                    <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Pricing</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/resources">
-                    <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Resources</p>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            <ul className='flex items-center gap-5'>
-              <li>
-                <Link href="/login">
-                  <p className="text-grayish-violet font-bold text-sm hover:text-very-dark-violet">Login</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/signup">
-                  <button
-                    className="bg-primary hover:opacity-60 text-white font-bold text-sm px-4 py-2 rounded-full hover:bg-opacity-80">
-                    Sign Up
-                  </button>
-                </Link>
-              </li>
-            </ul>
-
-          </nav>
-        </header>
+        <Header />        
 
         {children}
 
