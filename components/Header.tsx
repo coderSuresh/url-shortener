33f2b877161ff2f5b5ffd161eba9c32972c622ce
codersuresh <sd807141@gmail.com>
2023-07-06 22:42:50 +0545

    feat: add hero section and fix some layout issues in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false)
 
     return (
-        <header className='sm:px-10 py-4 px-5 flex relative justify-start items-center'>
+        <header className='lg:px-32 md:px-16 py-10 px-5 flex relative justify-start items-center'>
             <Link href="/">
                 <Image src="/images/logo.svg" width="100" height="30" alt='logo' />
             </Link>
@@ -23,7 +23,7 @@ const Header = () => {
                 sm:text-left text-center
                 sm:w-fit w-[calc(100%-40px)]
                 flex-col fixed sm:static
-                top-14 items-center
+                top-20 items-center
                 justify-between
                 sm:bg-transparent bg-dark-violet
                 sm:p-0 p-5
@@ -50,7 +50,7 @@ const Header = () => {
 
                 <div className="sm:hidden block divider h-[1px] w-full my-3 bg-grayish-violet" />
 
-                <ul className='flex sm:w-fit w-full items-center sm:flex-row flex-col sm:gap-5 gap-3 sm:absolute sm:right-10'>
+                <ul className='flex sm:w-fit w-full items-center sm:flex-row flex-col sm:gap-5 gap-3 sm:absolute lg:right-32 md:right-16 right-5'>
                     <li>
                         <Link href="/login">
                             <p className="sm:text-grayish-violet text-white font-bold text-sm sm:hover:text-very-dark-violet">Login</p>
