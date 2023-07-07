bf938909d7fb68acf9f511d698aafbe5dcea6dfc
codersuresh <sd807141@gmail.com>
2023-07-07 16:37:33 +0545

    fix: add max-w-full to prevent overflow of input

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -10,8 +10,8 @@ const Shorten = () => {
         <div className="shortner_section relative lg:px-32 md:px-16 px-5 md:-top-10 -top-16 md:mb-10 mb-5">
             <div className="shortner_container rounded-md text-center bg-dark-violet bg-no-repeat md:bg-cover bg-contain bg-right-top w-full md:bg-bg-shorten-desktop bg-bg-shorten-mobile px-8 py-6">
                 <form onSubmit={handleFormSubmit} action="#" className="flex flex-wrap gap-x-5 gap-y-3 md:flex-row flex-col">
-                    <input type="text" className="md:flex-grow px-4 py-2 rounded-md" placeholder="Shorten a link here..." />
-                    <button type="submit" className="md:flex-1 whitespace-nowrap px-4 py-2 rounded-md text-white bg-primary hover:opacity-70">
+                    <input type="text" className="px-4 max-w-full flex-grow py-2 rounded-md" placeholder="Shorten a link here..." />
+                    <button type="submit" className="whitespace-nowrap px-4 py-2 rounded-md text-white bg-primary hover:opacity-70">
                         Shorten It!
                     </button>
                 </form>
