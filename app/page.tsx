d9f41c88a340ef38a64a2719556738ca898b2a92
codersuresh <sd807141@gmail.com>
2023-07-07 20:48:47 +0545

    refactor: separate boost section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
-import BtnGetStarted from '@/components/BtnGetStarted'
+import BoostSection from '@/components/BoostSection'
 
 const HomePage = () => {
   return (
@@ -8,13 +8,7 @@ const HomePage = () => {
 
       <Hero />
       <Features />
-
-      <section className="boost lg:px-32 md:px-16 px-5 bg-dark-violet md:bg-bg-boost-desktop bg-bg-boost-mobile text-white py-10 text-center">
-        <h2 className='font-bold md:text-4xl text-3xl mb-4'>Boost your links today</h2>
-
-        <BtnGetStarted />
-
-      </section>
+      <BoostSection />
 
     </main>
   )
