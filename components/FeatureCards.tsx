a7a4cfe4a803cf6241de071911b89e721c5f518e
codersuresh <sd807141@gmail.com>
2023-07-07 16:33:14 +0545

    rafactor: reuse inner content of feature card

--- components/FeatureCards.tsx
+++ components/FeatureCards.tsx
@@ -1,38 +1,34 @@
+import InnerContentOfFeatureCards from "./InnerContentOfFeatureCards"
+
 const FeatureCards = () => {
     return (
         <div className="features flex md:flex-row flex-col justify-center md:items-start items-center mt-20 mb-10 pb-10">
             <div className="feature_card lg:px-8 md:px-4 px-8 w-[300px] bg-white rounded-md md:text-xl md:text-start text-center font-bold text-lg">
-                <div className="feature_card_icon bg-dark-violet w-16 p-4 md:mx-0 mx-auto rounded-full -mt-8">
-                    <img src="/images/icon-brand-recognition.svg" alt="brand recognition icon" aria-hidden="true" role='presentation' />
-                </div>
-                <h3 className='my-4 text-lg'>Brand Recognition</h3>
-                <p className="text-grayish-violet text-sm font-light pb-8">
-                    Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.
-                </p>
+                <InnerContentOfFeatureCards
+                    img='icon-brand-recognition.svg'
+                    title='Brand Recognition'
+                    desc='Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+                />
             </div>
 
             <div className="line w-6 h-[6px] bg-primary md:mt-32 md:rotate-0 rotate-90 mt-[10px]" />
 
             <div className="feature_card lg:px-8 md:px-4 px-8 w-[300px] bg-white rounded-md md:text-xl md:text-start text-center font-bold text-lg mt-[42px]">
-                <div className="feature_card_icon bg-dark-violet w-16 p-4 md:mx-0 mx-auto rounded-full -mt-8">
-                    <img src="/images/icon-detailed-records.svg" alt="brand recognition icon" aria-hidden="true" role='presentation' />
-                </div>
-                <h3 className='my-4 text-lg'>Detailed Records</h3>
-                <p className="text-grayish-violet text-sm font-light pb-8">
-                    Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.
-                </p>
+                <InnerContentOfFeatureCards
+                    img='icon-detailed-records.svg'
+                    title='Detailed Records'
+                    desc='Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'
+                />
             </div>
 
             <div className="line w-6 h-[6px] bg-primary md:mt-32 md:rotate-0 rotate-90 mt-[10px]" />
 
             <div className="feature_card lg:px-8 md:px-4 px-8 w-[300px] bg-white rounded-md md:text-start text-center font-bold md:mt-24 mt-[42px]">
-                <div className="feature_card_icon bg-dark-violet w-16 p-4 md:mx-0 mx-auto rounded-full -mt-8">
-                    <img src="/images/icon-fully-customizable.svg" alt="brand recognition icon" aria-hidden="true" role='presentation' />
-                </div>
-                <h3 className='my-4 text-lg'>Fully Customizable</h3>
-                <p className="text-grayish-violet text-sm font-light pb-8">
-                    Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.
-                </p>
+                <InnerContentOfFeatureCards
+                    img='icon-fully-customizable.svg'
+                    title='Fully Customizable'
+                    desc='Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'
+                />
             </div>
 
         </div>
---
+++ components/InnerContentOfFeatureCards.tsx
@@ -0,0 +1,21 @@
+type Props = {
+    img: string,
+    title: string,
+    desc: string
+}
+
+const InnerContentOfFeatureCards = ({ img, title, desc }: Props) => {
+    return (
+        <>
+            <div className="feature_card_icon bg-dark-violet w-16 p-4 md:mx-0 mx-auto rounded-full -mt-8">
+                <img src={`/images/${img}`} alt="brand recognition icon" aria-hidden="true" role='presentation' />
+            </div>
+            <h3 className='my-4 text-lg'>{title}</h3>
+            <p className="text-grayish-violet text-sm font-light pb-8">
+                {desc}
+            </p>
+        </>
+    )
+}
+
+export default InnerContentOfFeatureCards
