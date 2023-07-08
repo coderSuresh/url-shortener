7c30f136a517a3baaaec52cc90218a82f73893c2
codersuresh <sd807141@gmail.com>
2023-07-08 12:49:51 +0545

    fix: add margin to feature content

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -6,7 +6,7 @@ const Features = () => {
         <section className="feature bg-light-gray mt-28">
             <Shorten />
 
-            <div className="feature-content lg:px-32 md:px-16 px-5">
+            <div className="feature-content lg:px-32 md:px-16 px-5 mt-20">
                 <div className="text-center w-full max-w-[500px] mx-auto">
                     <h2 className='md:text-4xl text-2xl font-bold'>Advanced Statistics</h2>
                     <p className="text-grayish-violet mt-2">
