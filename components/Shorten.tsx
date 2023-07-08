98fbd24a43b7ee965317cd540932b4815de23113
codersuresh <sd807141@gmail.com>
2023-07-08 12:38:01 +0545

    refactor: remove margin from shorten

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -7,7 +7,7 @@ const Shorten = () => {
     }
 
     return (
-        <div className="shortner_section relative lg:px-32 md:px-16 px-5 md:-top-10 -top-16 md:mb-10 mb-5">
+        <div className="shortner_section relative lg:px-32 md:px-16 px-5 md:-top-10 -top-16">
             <div className="shortner_container rounded-md text-center bg-dark-violet bg-no-repeat md:bg-cover bg-contain bg-right-top w-full md:bg-bg-shorten-desktop bg-bg-shorten-mobile px-8 py-6">
                 <form onSubmit={handleFormSubmit} action="#" className="flex flex-wrap gap-x-5 gap-y-3 md:flex-row flex-col">
                     <input type="text" className="px-4 max-w-full flex-grow py-2 rounded-md" placeholder="Shorten a link here..." />
