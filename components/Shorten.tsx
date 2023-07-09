3c1af2bbb90d52039d25bd0f8207151e6a77544b
codersuresh <sd807141@gmail.com>
2023-07-09 15:01:09 +0545

    fix: add margin to shorten btn only on error

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -38,7 +38,7 @@ const Shorten = () => {
         <>
             <div className="shortner_section relative lg:px-32 md:px-16 px-5 md:-top-14 md:-mb-10 -top-20 -mb-16">
                 <div className="shortner_container rounded-md text-center bg-dark-violet bg-no-repeat md:bg-cover bg-contain bg-right-top w-full md:bg-bg-shorten-desktop bg-bg-shorten-mobile px-10 py-10">
-                    <form onSubmit={handleFormSubmit} action="#" className="relative flex flex-wrap gap-x-5 gap-y-8 md:flex-row flex-col">
+                    <form onSubmit={handleFormSubmit} action="#" className="relative flex flex-wrap gap-x-5 gap-y-3 md:flex-row flex-col">
                         <div className="flex-grow">
                             <input
                                 value={inputValue}
@@ -53,7 +53,7 @@ const Shorten = () => {
                                 </p>
                             )}
                         </div>
-                        <button type="submit" className="whitespace-nowrap px-4 h-fit py-2 rounded-md text-white bg-primary hover:opacity-70">
+                        <button type="submit" className={`whitespace-nowrap px-4 h-fit py-2 rounded-md text-white bg-primary hover:opacity-70 ${hasError ? 'mt-5' : ''}`}>
                             Shorten It!
                         </button>
                     </form>
