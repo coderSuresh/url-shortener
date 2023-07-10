e834fc679ab62f886600af85a0d0ef99805cf7cc
codersuresh <sd807141@gmail.com>
2023-07-10 14:23:25 +0545

    fix: add aria-label to make sure link has discernible text

--- components/Footer/Socials.tsx
+++ components/Footer/Socials.tsx
@@ -4,16 +4,16 @@ const Socials = () => {
     return (
         <div className='flex flex-col gap-y-5'>
             <div className='flex gap-x-5'>
-                <Link href='#' className='hover:text-primary'>
+                <Link href='#' className='hover:text-primary' aria-label='facebook'>
                     <i className="fab fa-facebook-square text-2xl"></i>
                 </Link>
-                <Link href='#' className='hover:text-primary'>
+                <Link href='#' className='hover:text-primary' aria-label='twitter'>
                     <i className="fab fa-twitter text-2xl"></i>
                 </Link>
-                <Link href='#' className='hover:text-primary'>
+                <Link href='#' className='hover:text-primary' aria-label='pinterest'>
                     <i className="fab fa-pinterest text-2xl"></i>
                 </Link>
-                <Link href='#' className='hover:text-primary'>
+                <Link href='#' className='hover:text-primary' aria-label='instagram'>
                     <i className="fab fa-instagram text-2xl"></i>
                 </Link>
             </div>
