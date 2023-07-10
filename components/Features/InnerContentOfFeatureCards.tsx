f657961511ade583868dd79397082a5eeaa2d04f
codersuresh <sd807141@gmail.com>
2023-07-10 14:00:28 +0545

    fix: replace img with Image from next

--- components/Features/InnerContentOfFeatureCards.tsx
+++ components/Features/InnerContentOfFeatureCards.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 type Props = {
     img: string,
     title: string,
@@ -8,7 +9,7 @@ const InnerContentOfFeatureCards = ({ img, title, desc }: Props) => {
     return (
         <>
             <div className="feature_card_icon bg-dark-violet w-16 p-4 md:mx-0 mx-auto rounded-full -mt-8">
-                <img src={`/images/${img}`} alt="brand recognition icon" aria-hidden="true" role='presentation' />
+                <Image src={`/images/${img}`} alt="brand recognition icon" aria-hidden="true" role='presentation' />
             </div>
             <h3 className='my-4 text-lg'>{title}</h3>
             <p className="text-grayish-violet text-sm font-light pb-8">
