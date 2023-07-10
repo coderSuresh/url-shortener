15abd6499aa889a6a4845b0323d22bece35ef81d
codersuresh <sd807141@gmail.com>
2023-07-10 14:37:41 +0545

    change color of original text to dark blue

--- components/ShortenLink.tsx
+++ components/ShortenLink.tsx
@@ -44,7 +44,7 @@ const ShortenLink = ({ originalLink, shortenLink, shortenedLinks, setShortenedLi
     return (
         <div className="shortened_links rounded bg-white mx-5 lg:mx-32 md:mx-16 mb-4">
             <div className="px-4 py-2 flex md:flex-row flex-col md:flex-wrap gap-5 gap-y-2 md:items-center justify-end">
-                <p className="link break-words flex-grow">
+                <p className="link break-words flex-grow text-very-dark-blue">
                     {originalLink}
                 </p>
                 <div className="divider h-[1px] w-full bg-light-gray md:hidden block" />
